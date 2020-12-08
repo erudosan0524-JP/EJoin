a2ba1ce9d46e5a4e91f5278288793ddde6448663
erudosan0524-JP <Eruvis4869>
2020-12-08 14:56:01 +0900

    [add] add main method
    メインメソッドの追加

---
+++ src/main/java/click/erudosaba/mc/ejoin/Main.java
@@ -0,0 +1,16 @@
+package click.erudosaba.mc.ejoin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
